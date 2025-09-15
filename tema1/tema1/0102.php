701a678d0117546c11e53ae7053be62f6382475c
mawah5035 <mariam_wahedi@icloud.com>
2025-09-15 13:26:02 +0200

    n

--- tema1/tema1/0102.php
+++ tema1/tema1/0102.php
@@ -7,8 +7,8 @@
 $tall1= $_POST ["tall1"]; 
 $tall2= $_POST ["tall2"];
 
-$summen=$tall1 + $tall2
-$differansen=$tall1-$tall2
+$summen= $tall1 + $tall2
+$differansen= $tall1-$tall2
 
 print ("tall 1 er $tall1 <br />")
 print ("tall 2 er $tall2 <br />")

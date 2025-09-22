c039586a9ace44be8a7c49f00faf11bf03079deb
mawah5035 <mariam_wahedi@icloud.com>
2025-09-22 13:39:17 +0200

    endring i oppgave 1

--- tema2/0201.php
+++ tema2/0201.php
@@ -1,10 +1,15 @@
 <?php    /* Eksempel 1 */
 /*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet skriver ut en "god dag"-melding med personens navn 
+/*    Programmet mottar fra et HTML-skjema et svar på spørsmål "hva er 3 ganger 3?"
+/*    Programmet skjekker om svaret er riktig og skriver ut en melding ang svaret 
 */
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
-	
-  print ("God dag $fornavn $etternavn <br />");  
+  $svar=$_POST ["svar"];
+  if ($svar==9) 
+    {
+    print("Riktig. 3 ganger 3 er 9");
+  } 
+  else {
+    print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9.");
+  }
+	 
 ?>

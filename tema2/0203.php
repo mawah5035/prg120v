31f96178d5ac2015e8d3edefaf8e773fbb589f69
mawah5035 <mariam_wahedi@icloud.com>
2025-09-22 14:05:16 +0200

    n

--- tema2/0203.php
+++ tema2/0203.php
@@ -1,17 +1,22 @@
-<?php    /* Eksempel 3 */
+<?php    /* Oppgave 2 */
 /*
-/*    Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
-/*    Programmet skriver ut de 2 tallene og summen og differansen av/mellom tallene
+/*    Programmet mottar fra et html-skjema et svar på spørsmålet "Er du student (j/n)?"
+/*    Programmet skjekker om det er svart j eller n på spørsmålet og skrivver ut en passende melding
 */
-  $tall1=$_POST ["tall1"];
-  $tall2=$_POST ["tall2"];  /* variable gitt verdier fra feltene i HTML-skjemaet */
-	
-  $sum=$tall1 + $tall2   /* beregning av summen utført  */
-  $differanse=$tall1 - $tall2;  /* beregning av differansen utført  */
-			
-  print ("Tall 1 er $tall1 <br />");
-  print ("Tall 2 er $tall2 <br />");
-  print ("<br />");
-  print ("Summen er $sum <br />");
-  print ("Differansen er $differanse <br />");  /* resultater skrevet ut  */
-?>
+$svar=$_POST["svar"];
+if (!$svar) /*ikke svar på spørsmålet */
+  {
+  print("Du har ikke svart på spørsmålet <br/>");
+
+}
+else if ($svar=="ja"|| $svar=="JA"|| $svar=="j"||$svar=="J" || $svar=="Ja") {
+  print("Du har svart ja på spørsmålet om du er student <br/>"); 
+}
+else if ($svar=="nei" ||$svar=="NEI"||$svar=="Nei"||$svar=="n"||$svar=="N") 
+  {
+  print("Du har svart nei på spørsmålet om du er student <br/>");
+  }
+  else {
+    print("Du har ikke svart ja eller nei på spørsmålet om du er student <br/>");
+  }
+  ?>

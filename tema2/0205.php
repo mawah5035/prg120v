59780253d82ea1347fece7fc37afe2485071ce4d
mawah5035 <mariam_wahedi@icloud.com>
2025-09-29 19:41:02 +0200

    php 05

--- tema2/0205.php
+++ tema2/0205.php
@@ -1,1 +1,47 @@
 <?php
+
+$tall1=$_POST ["tall1"];
+$tall2=$_POST ["tall2"];
+$tall3=$_POST["tall3"];
+
+if ($tall3==1) 
+    {
+    $resultat=$tall1+$tall2;
+
+    print ("Tall 1 er $tall1 <br/>");
+    print ("Tall2 er $tall2 <br/>");
+    print ("Regneoperasjonen er addisjon<br/>");
+    print ("Resultatet av regneoperasjonen er $resultat<br/>");
+}
+else if ($tall3==2) 
+    {
+        $resultat=$tall1-$tall2;
+
+    print ("Tall 1 er $tall1 <br/>");
+    print ("Tall2 er $tall2 <br/>");
+    print ("Regneoperasjonen er subtraksjon <br/>");
+    print ("Resultatet av regneoperasjonen er $resultat<br/>");
+}
+else if ($tall3==3) 
+    {
+        $resultat=$tall1*$tall2;
+
+    print ("Tall 1 er $tall1 <br/>");
+    print ("Tall2 er $tall2 <br/>");
+    print ("Regneoperasjonen er multiplikasjon<br/>");
+    print ("Resultatet av regneoperasjonen er $resultat<br/>");
+}
+else if ($tall3==4) 
+    {
+        $resultat=$tall1/$tall2;
+
+    print ("Tall 1 er $tall1 <br/>");
+    print ("Tall2 er $tall2 <br/>");
+    print ("Regneoperasjonen er divisjon<br/>");
+    print ("Resultatet av regneoperasjonen er $resultat<br/>");
+}
+else 
+    {
+    print("Det er ikke angitt en gyldig regneoperasjon");
+}
+?>

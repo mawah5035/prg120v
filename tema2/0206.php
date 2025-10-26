cec6ca9d12af7d4044df75055b212f0cd5971560
mawah5035 <mariam_wahedi@icloud.com>
2025-10-26 16:22:26 +0100

    oppg 6

--- tema2/0206.php
+++ tema2/0206.php
@@ -1,2 +1,15 @@
  <?php
- 
+
+$tall1 = $_POST['tall1'];
+$tall2 = $_POST['tall2'];
+
+if ($tall1 > $tall2) {
+    print("$tall1 er større enn $tall2");
+}
+else if ($tall1 < $tall2) {
+    print("$tall2 er større enn $tall1");
+}
+else if ($tall1 == $tall2) {
+    print("$tall1 er lik $tall2");
+}
+?>

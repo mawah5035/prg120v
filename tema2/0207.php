70df29812f511bcdc496653d6825e365b2a9f94c
mawah5035 <mariam_wahedi@icloud.com>
2025-10-26 16:52:07 +0100

    n

--- tema2/0207.php
+++ tema2/0207.php
@@ -10,45 +10,45 @@ print ("Tall 1 er $tall1 <br> Tall 2 er $tall2 <br> Tall 3 er $tall3 <br>");
 
 if ($tall1 > $tall2 )
     {
-        print ("<br> $tall1 er større enn $tall2");
+        print ("<br> Tall 1 er større enn Tall 2");
     }
 else if ($tall1 < $tall2 )
     {
-        print ("<br> $tall1 er mindre enn $tall2");
+        print ("<br> Tall 1 er mindre enn Tall 2");
     }
 else if ($tall1 == $tall2 )
     {
-        print ("<br> $tall1 er lik $tall2");
+        print ("<br> Tall 1 er lik Tall 2");
     }
  
 
 
 if      ($tall2 > $tall3 )
     {
-        print ("<br> $tall2 er større enn $tall3");
+        print ("<br> Tall 2 er større enn Tall 3");
     }
 
 else if ($tall2 == $tall3) 
 {
-    print ("<br>$tall2 er lik $tall3");
+    print ("<br> Tall 2 er lik Tall 3");
 }
 else if ($tall2 < $tall3 )
     {
-        print ("<br> $tall2 er mindre enn $tall3");
+        print ("<br> Tall 2 er mindre enn tall 3");
     }
 
 
 
 if ($tall1 > $tall3 )
     {
-        print ("<br> $tall1 er større enn $tall3");
+        print ("<br> Tall 1 er større enn Tall 3");
     }
 else if ($tall1 < $tall3 )
     {
-        print ("<br> $tall1 er mindre enn $tall3");
+        print ("<br> Tall 1 er mindre enn Tall 3");
     }
 else if ($tall1 == $tall3)
     {
-        print ("<br> $tall1 er lik $tall3");
+        print ("<br> Tall 1 er lik Tall 3");
     }
 ?>

e848b326718768f8dfdfd528060d29c667c18e76
mawah5035 <mariam_wahedi@icloud.com>
2025-10-26 16:57:54 +0100

    oppgave 8

--- tema2/0201.php
+++ tema2/0201.php
@@ -13,3 +13,18 @@
   }
 	 
 ?> /* Slutten av php */
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hva er 3 ganger 3?</title>
+</head>
+<body>
+  <h3>Oppgave 1 i tema 2</h3>
+    
+  <form method="post" action="" id="eksempel1" name="eksempel1">
+    Hva er 3 ganger 3? <input type="text" id="svar" name="svar" required /> <br />
+    <input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
+    <input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+  </form>
+</body>
+</html>

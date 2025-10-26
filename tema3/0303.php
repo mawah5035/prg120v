fef663b0cbf8c74d31c991686be8fecb6afc0236
mawah5035 <mariam_wahedi@icloud.com>
2025-10-26 19:19:44 +0100

    oppgave 3

---
+++ tema3/0303.php
@@ -0,0 +1,19 @@
+<?php
+
+for ($tall=1; $tall<=10 ; $tall++) 
+{ 
+print "$tall<br>";
+}
+print "<br>";
+
+for ($tall=11; $tall<=20 ; $tall++) 
+{
+    print "$tall<br>";
+}
+print "<br>";
+
+for ($tall=21; $tall<=30 ; $tall++) 
+{
+    print "$tall<br>";
+}
+?>

009a72b101c0fbee5eca19f77f60fcb250edf05a
mawah5035 <mariam_wahedi@icloud.com>
2025-10-26 17:01:17 +0100

    n

--- tema2/0201.php
+++ tema2/0201.php
@@ -1,14 +1,12 @@
-<?php    /* Eksempel 1 */
-/*
-/*    Programmet mottar fra et HTML-skjema et svar på spørsmål "hva er 3 ganger 3?"
-/*    Programmet skjekker om svaret er riktig og skriver ut en melding ang svaret 
-*/
-  $svar=$_POST ["svar"]; /* Henter data fra skjemaet, her henter den inn svaret som har blitt sendt inn og lagrer det som svar */
-  if ($svar==9) /* om svaret som er sendt inn er 9, vil første print bli printet */
+<?php  
+
+  $svar=$_POST ["svar"]; 
+
+  if ($svar==9) 
     {
     print("Riktig. 3 ganger 3 er 9");
   } 
-  else { /*hvis ikke, vil andre print bli printet */
+  else { 
     print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9.");
   }	 
 ?> 

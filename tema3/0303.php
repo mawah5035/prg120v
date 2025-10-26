f353867f088042c5563756eba2fc183ca6aaf71f
mawah5035 <mariam_wahedi@icloud.com>
2025-10-26 19:23:26 +0100

    m

--- tema3/0303.php
+++ tema3/0303.php
@@ -2,18 +2,18 @@
 
 for ($tall=1; $tall<=10 ; $tall++) 
 { 
-print "$tall";
+print "$tall ";
 }
 print "<br>";
 
 for ($tall=11; $tall<=20 ; $tall++) 
 {
-    print "$tall";
+    print "$tall ";
 }
 print "<br>";
 
 for ($tall=21; $tall<=30 ; $tall++) 
 {
-    print "$tall";
+    print "$tall ";
 }
 ?>

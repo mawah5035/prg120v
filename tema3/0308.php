63725490f1f4e0802e20a697361ef7e19ff7cc44
mawah5035 <mariam_wahedi@icloud.com>
2025-10-27 13:05:17 +0100

    oppgave 8 tema 3

---
+++ tema3/0308.php
@@ -0,0 +1,23 @@
+<?php
+
+$tall1=$_POST["tall1"];
+$tall2=$_POST["tall2"];
+$tall3=$_POST["tall3"];
+$tall4=$_POST["tall4"];
+$tall5=$_POST["tall5"];
+
+print("Dine fem tall er $tall1 $tall2 $tall3 $tall4 $tall5 <br>");
+
+$tallArray = array($tall1, $tall2, $tall3, $tall4, $tall5);
+
+for ($tall=0; $tall < ; $tall++) 
+{ 
+    print("$tallArray[$tall] <br>");
+}
+print("<br>");
+
+for ($tall=4; $tall>= 0;, $tall--) 
+{ 
+    print("$tallArray[$tall] <br>");
+}
+?>

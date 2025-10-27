92e620fadc244d0a3aa1d553135841d86c213e65
mawah5035 <mariam_wahedi@icloud.com>
2025-10-27 11:59:59 +0100

    n

--- tema3/0305.php
+++ tema3/0305.php
@@ -8,5 +8,6 @@ for ($tall=1; $tall <=10 ; $tall++)
      $sum = $sum + $tall;
     $gjennomsnitt = $sum / 10;
 }
-print("Summen av tallene fra 1 til 10 er $sum <br> Gjennomsnittet av tallene fra1 til 10 er $differanse");
+
+print("Summen av tallene fra 1 til 10 er $sum <br> Gjennomsnittet av tallene fra1 til 10 er $gjennomsnitt <br>");
     ?>

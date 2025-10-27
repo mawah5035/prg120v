7ad82826259004e198d080b0145a503f160ccce7
mawah5035 <mariam_wahedi@icloud.com>
2025-10-27 11:58:57 +0100

    oppgave 5 tema 3

---
+++ tema3/0305.php
@@ -0,0 +1,12 @@
+<?php
+
+$sum = 0;
+$gjennomsnitt = 0;
+
+for ($tall=1; $tall <=10 ; $tall++) 
+{ 
+     $sum = $sum + $tall;
+    $gjennomsnitt = $sum / 10;
+}
+print("Summen av tallene fra 1 til 10 er $sum <br> Gjennomsnittet av tallene fra1 til 10 er $differanse");
+    ?>

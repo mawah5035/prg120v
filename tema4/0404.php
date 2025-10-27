de9cb874261b5ef4b711dacb16aa44a973184b60
mawah5035 <mariam_wahedi@icloud.com>
2025-10-27 18:57:04 +0100

    oppgave 4 tema 4

---
+++ tema4/0404.php
@@ -0,0 +1,44 @@
+<?php
+
+function validerPostnr($postnr) 
+// Hensikten til funksjonener å skjekke om postnret som er fylt ut er korrekt
+// Parameter: $postnr - postnummeret skal skjekkes 
+// funksjonsverdi/returverdi, funksjonen returnerer true hvis post nummeret er korrekt
+// funksjonen returerer false ellers 
+
+{
+    $lovligPostnr=true;
+
+    if (!$postnr) 
+    {
+        $lovligPostnr=false; 
+    }
+
+    else if (strlen($postnr)!=4) 
+    {
+        $lovligPostnr=false;    
+    }
+    else if (!ctype_digit($postnr)) 
+    {
+        $lovligPostnr=false;    
+    }
+
+    returne $lovligPostnr; // returnerer true eller false og er slutt på funksjonen   
+}
+
+$postnr=$_POST["postnr"];
+
+$lovligPostnr=validerPostnr($postnr); //valideringsfunksjon
+
+if ($lovligPostnr) //om den er korrekt
+{
+    print("Postnr er korrekt fylt ut"<br>);
+}
+else //om det ikke er korrekt
+{
+    print("Postnr er ikke korrekt utfylt")
+}
+?>
+
+
+

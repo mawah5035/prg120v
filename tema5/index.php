9fc7ac66167cdd42b68b574c85a3b42b2edaf0d2
mawah5035 <mariam_wahedi@icloud.com>
2025-10-29 13:34:03 +0100

    n

---
+++ tema5/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tema 5 oppgaver</title>
+</head>
+<body>
+    <h1>Databasehåndtering</h1>
+    
+    <h2> Studium: </h2>
+    <ul>
+    <li><a href="registrerstudium.php"><button>Registere klasse</button></a></li>
+    <li><a href="visallestudiumer.php"><button>Vis alle klasser</button></a></li>
+    <li><a href="slettstudium.php"><button>Slett klasse</button></a></li>
+    </ul>
+
+    <h2> Emner: </h2>
+    <ul>
+    <li><a href="registreremne.php"><button>Registere studenter</button></a></li>
+    <li><a href="visalleemner.php"><button>Vis alle studenter</button></a></li>
+    <li><a href="slettemne.php"><button>Slett student</button></a></li>
+    </ul>
+
+</body>
+</html>

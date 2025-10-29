166433ec38cd04aab6c0e69e0daaa97af571732a
mawah5035 <mariam_wahedi@icloud.com>
2025-10-29 15:47:34 +0100

    n

--- tema5/visalleemner.php
+++ tema5/visalleemner.php
@@ -2,8 +2,6 @@
 
 include("db-tilkobling.php"); //Inkluderer database tilkoblings filen
 
-<h3> Vis alle emner </h3> //Printer en overskrift
-
 $sqlSetning= "SELECT * FROM emne ORDER BY emnekode;"; //Lager en sql setning som henter alle emner
 $sqlResultat= mysqli_query($db, $sqlSetning) or die; //Kjører sql setningen mot databasen som er koblet til, sql resultatet er svaret fra databasen
 
@@ -13,13 +11,13 @@ Print ("<h2>Registerte emner: </h2>"); //Printer en overskrift
 Print ("<table border=1>"); //Starter en tabell med border
 Print ("<tr><th align=left>Emnekode</th><th align=left>Emnenavn</th></tr>"); //Printer tabell overskrifter, tr for tabell row, th for tabell header
 
-for ($r=1; $r<=$antallRader; $r+) 
+for ($r=1;$r<=$antallRader;$r++) 
 { 
      $rad= mysqli_fetch_array($sqlResultat); //Henter en rad av gangen fra sql resultatet
      $emnekode= $rad["emnekode"]; //Henter emnekode fra raden
      $emnenavn= $rad["emnenavn"]; //Henter emnenavn fra raden
 
-     Print ("<tr><td>$emnekode</td><td>$emnenavn</td></tr>"); //Printer emnekode og emnenavn i tabell rader, td for tabell data
+     Print ("<tr><td>$emnekode</td> <td>$emnenavn</td></tr>"); //Printer emnekode og emnenavn i tabell rader, td for tabell data
 }
 Print ("</table>"); //Avslutter tabellen
 

5dce4e8d359c174c2543ec9c00ddadae7be9e230
mawah5035 <mariam_wahedi@icloud.com>
2025-10-30 15:34:04 +0100

    slett emne

---
+++ tema5/slettemne.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<script scr= "funksjoner.js"> </script>
+
+<h3>Slett emne</h3>
+
+<form method="post" action="" id=slettemne name=slettemne onSubmit="return bekreft ()" >
+    Hvilket emne ønsker du å slette? <input type= "text" id="emnekode" name="emnekode" required /> <br>
+    <input type="submit" value="Slett emne" id=submit name="submit">
+    <input type="reset" value="Nullstill" id="nullstill" name="nullstill">
+</form>
+
+<?php
+
+if (isset($_POST ["slettemne"])) 
+{
+  $emnekode=$_POST ["emnekode"];
+
+    include("db-tilkobling.php");
+
+    $sqlSetning="DELETE FROM emne WHERE emnekode='$emnekode';";
+    mysqli_query($db, $sqlSetning) or die ("ikke mulig å slette data i databasen");
+
+    Print ("Følgende emnekode er nå slettet: $emnekode $emnenavn");
+}
+
+?>
+  
+

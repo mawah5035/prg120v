dafbc27764a474957f29d525b6c17eb17b6a695d
mawah5035 <mariam_wahedi@icloud.com>
2025-10-30 23:10:37 +0100

    n

--- tema5/registreremne.php
+++ tema5/registreremne.php
@@ -6,39 +6,42 @@
 <form method="post" action="" id="registrerEmne" name="registrerEmne" >
     Emnekode: <input type= "text" id="emnekode" name="emnekode" required /> <br>
     Emnenavn: <input type= "text" id="emnenavn" name="emnenavn" required /><br>
-    <input type="submit" value="Send inn" id="submit" name="submit"/>
+    Studiumkode: <input type= "text" id="studiumkode" name="studiumkode" required /><br>
+    <input type="submit" value="Send inn" id="registrerEmne" name="registrerEmne"/>
     <input type="reset" value= "Nullstill" id="nullstill" name="nullstill"/>
 </form>
 
 <?php
 
-if (isset($_POST ["submit"])) 
+if (isset($_POST ["registrerEmne"])) // trim er for å hindre feil ved sammenligning i db, sørger for at input er ren og reduserer risiko for uventet oppførsel
 {
-    $emnekode=$_POST ["emnekode"];
-    $emnenavn=$_POST ["emnenavn"];
+    $emnekode=trim($_POST ["emnekode"]);
+    $emnenavn=trim($_POST ["emnenavn"]);
+    $studiumkode=trim($_POST ["studiumkode"]);
 
-    if (!$emnekode || !$emnenavn) 
+    if (!$emnekode || !$emnenavn || !$studiumkode) 
     {
-        Print ("Både emne kode og emne navn må fylles ut");
+        echo ("Både emne kode og emne navn må fylles ut");
     }
+
     else 
     {
         include("db-tilkobling.php");
 
-        $sqlSetning= "SELECT * FROM emne WHERE emnekode= '$emnekode'"; 
-        $sqlResultat=mysqli_query($db, $sqlSetning) or die ("ikke mulig å hente data fra databasen");
-        $antallRader=mysqli_num_rows($sqlResultat);
+        $skjekk = "SELECT * FROM emne WHERE emnekode='$emnekode'";
+        $resultat = mysqli_query($db, $skjekk);
 
-        if ($antallRader!=0) 
-        {
-            Print ("Emner registerert fra før");
-        }
-        else 
-        {
-            $sqlSetning= "INSERT INTO emne VALUES ('$emnekode', '$emnenavn');";
+        if (mysqli_num_rows($resultat) > 0) {
+            echo ("$emnekode er registerert fra før");
+        } else {
+            $sqlSetning= "INSERT INTO emne (emnekode, emnenavn, studiumkode)
+                           VALUES ('$emnekode', '$emnenavn', '$studiumkode')";
 
-            Print ("Følgende emne er nå registert: $emnekode $emnenavn");
+            echo ("Følgende emne er nå registert: $emnekode $emnenavn $studiumkode");
+        } else {
+            echo "Feil ved registereing". mysqli_error($db);
         }
     }
 
 }
+?>

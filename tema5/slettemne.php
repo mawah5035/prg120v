4d0accb81615e163e198c81146cde5c8d17dc2eb
mawah5035 <mariam_wahedi@icloud.com>
2025-10-30 21:18:27 +0100

    slett emne

--- tema5/slettemne.php
+++ tema5/slettemne.php
@@ -1,24 +1,39 @@
 <?php
+include("db-tilkobling.php"); //inkluderer ekstern php
 ?>
 
-<script scr= "funksjoner.js"> </script>
-
-<h3>Slett emne</h3>
+<h3>Slett emne</h3> 
 
 <form method="post" action="" id=slettemne name=slettemne onSubmit="return bekreft ()" >
-    Hvilket emne ønsker du å slette? <input type= "text" id="emnekode" name="emnekode" required /> <br>
-    <input type="submit" value="Slett emne" id=submit name="submit">
-    <input type="reset" value="Nullstill" id="nullstill" name="nullstill">
+    <select name="emnekode" id="emnekode" required>
+        <option value="" >--Velg emne---</option>
+
+<?php //starter ny php
+$sqlSetning="SELECT * FROM emne ORDER BY emnekode"; //sql setningen henter emnekodene som er lagt inn i databasen
+$resultat= mysqli_query($db, $sql); //resultatet blir at lagret i $resultater, db er koblingen til databasen og sql inneholder spørringen som henter emnekode og emnenavn fra tabellen. 
+
+while ($rad= mysqli_fetch_array($resultat)) //while går igjennom hver rad i resuktatet fra databasen, mysqli_fetch_array henter en rad om gangen som et assosiastivt array. 
+{
+    // Her hentes ut verdiene fra emnekode og emnenavn fra den aktuelle raden
+    $emnekode=$rad["emnekode"];
+    $emnenavn=$rad["emnenavn"];
+    //skriver ut en option element som får brukeren til å kunne velge 
+    Print "<option value= '$emnekode'>$emnekode . $emnenavn</option>";
+}
+?>
+</select>
+
+<br><br>
+<input type="submit" value="Slett emne" id="submit" name="submit"/>
 </form>
 
-<?php
+
+
 
 if (isset($_POST ["submit"])) 
 {
   $emnekode=$_POST ["emnekode"];
   $emnenavn=$_POST ["emnenavn"];
-
-    include("db-tilkobling.php");
 
     $sqlSetning="DELETE FROM emne WHERE emnekode='$emnekode';";
     mysqli_query($db, $sqlSetning) or die ("ikke mulig å slette data i databasen");
